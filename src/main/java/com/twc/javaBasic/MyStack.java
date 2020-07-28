5ad5cb49b6225422bc431f5210aa813ce5804bf6
Annie <anqi.chen@thoughtworks.com>
2020-07-28 15:56:48 +0800

    commit java-basic-self-testing

--- src/main/java/com/twc/javaBasic/MyStack.java
+++ src/main/java/com/twc/javaBasic/MyStack.java
@@ -24,7 +24,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-
+        storage[count] = value;
+        count++;
         // --end-->
     }
 
@@ -37,7 +38,10 @@ public class MyStack {
         //  IMPORTANT: You SHOULD NOT USE COLLECTIONS (such as List<T>) OTHER THAN ARRAY.
         //
         // <--start
-
+        int[] newStorage = new int[newCapacity];
+        System.arraycopy(storage,0,newStorage,0,storage.length);
+        storage = newStorage;
+        capacity = newCapacity;
         // --end-->
     }
 
@@ -57,9 +61,30 @@ public class MyStack {
         //  Please pop one element from the array. You should update the `count` field
         //  as well.
         // <--start
-
+        if(storage.length == 0){
+            throw new UnsupportedOperationException("Stack is empty.");
+        }
+        int result = storage[count-1];
+        count--;
+        int[] new_storage = new int[count];
+        System.arraycopy(storage,0,new_storage,0,count);
+        storage = new_storage;
+        return result;
         // --end-->
 
-        throw new UnsupportedOperationException("Stack is empty.");
+    }
+
+    public static void main(String[] arg){
+        final int itemsCount = 25;
+        final int initialCapacity = 10;
+
+        // Hint: Please open MyStack.java
+        MyStack myStack = new MyStack(initialCapacity);
+        for (int i = 0; i < itemsCount; ++i) {
+            myStack.push(i);
+        }
+
+        int[] array = myStack.popToArray();
+        System.out.println(array);
     }
 }
--- src/test/java/com/twc/javaBasic/GenericTest.java
+++ src/test/java/com/twc/javaBasic/GenericTest.java
@@ -14,7 +14,7 @@ class GenericTest {
     //  The getMiddle method is a generic method. Now, please call getMiddle method for string
     //  type.
     // <--start
-    final String middle = null;
+    final String middle = getMiddle(words);
     // --end-->
 
     assertEquals("Good", middle);
@@ -52,8 +52,14 @@ class GenericTest {
   //  You should not change the signature of the function. But you can change
   //  the declaration of the generic type parameter.
   // <--start
-  private static <T> T min(T[] values) {
-    throw new RuntimeException("Not implemented");
+  private static <T extends Comparable> T min(T[] values) {
+    T res = values[0];
+    for (T val : values){
+      if(res.compareTo(val)>0){
+        res = val;
+      }
+    }
+    return res;
   }
   // --end-->
 
@@ -65,7 +71,12 @@ class GenericTest {
   //  A wildcard is not a type variable, so we can’t write code that uses ? as a type.
   // <--start
   private static void swap(Pair<?> pair) {
-    throw new RuntimeException("Not implemented");
+    Pair<?> res = new Pair<>(pair.getSecond(),pair.getFirst());
+    Object pleft = pair.getFirst();
+    Object pright = pair.getSecond();
+
+    pair.setFirst(pleft);
+    pair = res;
   }
 
   // TODO: You can add additional method within the range if you like
